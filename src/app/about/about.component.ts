6b7ffd13f2b0b14060544420b196d29b06d29a77
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-03-17 00:19:14 +0300

    Second major changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,6 +3,7 @@ import { TuiAppearance, TuiButton, TuiTitle } from '@taiga-ui/core';
 import { TuiAvatar, TuiAvatarStack, TuiChip, TuiProgress } from '@taiga-ui/kit';
 import { TuiCardLarge, TuiCardMedium, TuiCell } from '@taiga-ui/layout';
 import {TuiRipple} from '@taiga-ui/addon-mobile';
+import { NgxDotpatternComponent } from '@omnedia/ngx-dotpattern';
 
 @Component({
   selector: 'app-about',
@@ -17,7 +18,7 @@ import {TuiRipple} from '@taiga-ui/addon-mobile';
     TuiAppearance,
     TuiButton,
     TuiRipple,
-    TuiCardMedium
+    NgxDotpatternComponent
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.less'
