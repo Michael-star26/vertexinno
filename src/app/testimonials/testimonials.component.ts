52b3d62cc839ed80706b8c60c5cfbce7accf7e16
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-03-22 17:21:07 +0300

    Third main commit

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { NgSwitch, NgSwitchCase } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TuiAppearance, TuiButton, TuiIcon, TuiTitle } from '@taiga-ui/core';
+import { TuiAvatar, TuiCarousel, TuiRating, TuiSegmented } from '@taiga-ui/kit';
+import { TuiCardLarge, TuiCardMedium, TuiCell } from '@taiga-ui/layout';
 @Component({
   selector: 'app-testimonials',
   imports: [
-    
+    TuiCardLarge,
+    TuiCarousel,
+    TuiSegmented,
+    TuiAvatar,
+    NgSwitch,
+    NgSwitchCase,
+    TuiAppearance,
+    TuiTitle,
+    TuiCell,
+    TuiRating,
+    FormsModule,
+    TuiIcon,
   ],
+  changeDetection:ChangeDetectionStrategy.OnPush,
   templateUrl: './testimonials.component.html',
   styleUrl: './testimonials.component.less'
 })
 export class TestimonialsComponent {
-  
+  protected readonly index=signal(0)
+  protected readonly segmentedIndex=0
+
+  previous():void{
+    this.index.update((index)=>index-1)
+  }
+
+  next():void{
+    this.index.update((index)=>index+1)
+  }
 }
