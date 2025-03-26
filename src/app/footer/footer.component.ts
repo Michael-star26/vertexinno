423112b601c3f711a30c8c30c4b7bd03bfe1aaf0
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-03-27 01:48:26 +0300

    fifth commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TuiAppearance } from '@taiga-ui/core';
+import { TuiAppearance, TuiIcon, TuiTitle } from '@taiga-ui/core';
+import { TuiAvatar } from '@taiga-ui/kit';
+import { TuiCardLarge, TuiCardMedium, TuiCell, TuiHeader } from '@taiga-ui/layout';
 import { NgxScrollTopModule } from 'ngx-scrolltop';
 @Component({
   selector: 'app-footer',
   imports: [
     RouterOutlet,
     NgxScrollTopModule,
-    TuiAppearance
+    TuiAppearance,
+    TuiCardLarge,
+    TuiHeader,
+    TuiTitle,
+    TuiAvatar,
+    TuiCell,
+    TuiCardMedium,
+    TuiIcon
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.less'
