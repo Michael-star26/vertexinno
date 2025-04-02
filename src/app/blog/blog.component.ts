038868cfe5c6daadcc874527828adea0b78446ce
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-04-03 00:16:48 +0300

    mobile friendly

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
-
+import { TuiRipple } from '@taiga-ui/addon-mobile';
+import { TuiAppearance, TuiButton, TuiIcon, TuiTitle } from '@taiga-ui/core';
+import { TuiAvatar, TuiChip, TuiFade } from '@taiga-ui/kit';
+import { TuiCardLarge, TuiCardMedium, TuiCell, TuiHeader } from '@taiga-ui/layout';
+import { NgxFastMarqueeModule } from "ngx-fast-marquee";
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [
+    NgxFastMarqueeModule,
+    TuiCardLarge,
+    TuiAppearance,
+    TuiCardMedium,
+    TuiHeader,
+    TuiTitle,
+    TuiAvatar,
+    TuiIcon,
+    TuiButton,
+    TuiCell,
+    TuiRipple,
+    TuiFade,
+    TuiChip
+  ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.less'
 })
