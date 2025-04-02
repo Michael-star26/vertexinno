f4817fba87fa539df49bde22821dd995376b1872
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-04-02 18:18:03 +0300

    mobile friendly

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -4,7 +4,7 @@ import { TuiAvatar, TuiAvatarOutline, TuiChip } from '@taiga-ui/kit';
 import { TuiCardLarge, TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
 import { NgxTracingBeamComponent } from '@omnedia/ngx-tracing-beam';
 import { NgxShineBorderComponent } from '@omnedia/ngx-shine-border';
-
+import { NgxDotpatternComponent } from '@omnedia/ngx-dotpattern';
 @Component({
   selector: 'app-services',
   imports: [
@@ -17,7 +17,8 @@ import { NgxShineBorderComponent } from '@omnedia/ngx-shine-border';
     TuiTitle,
     TuiChip,
     NgxTracingBeamComponent,
-    NgxShineBorderComponent
+    NgxShineBorderComponent,
+    NgxDotpatternComponent
   ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.less'
