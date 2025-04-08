cb2d0a6aa8beea5dd700bb87436381b57cada206
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-04-08 19:00:17 +0300

    how we do things

--- src/app/howitworks/howitworks.component.ts
+++ src/app/howitworks/howitworks.component.ts
@@ -1,24 +1,48 @@
 import { Component } from '@angular/core';
 import { TuiAppearance, TuiTitle } from '@taiga-ui/core';
 import { TuiAvatar, TuiBadge, TuiChip } from '@taiga-ui/kit';
-import { TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
+import { TuiCardLarge, TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
 import { NgxFlickeringGridComponent } from '@omnedia/ngx-flickering-grid';
+import { NgxTimelineComponent, TimelineEntry } from '@omnedia/ngx-timeline';
+import { NgxNeonUnderlineComponent } from '@omnedia/ngx-neon-underline';
 
 @Component({
   selector: 'app-howitworks',
   imports: [
     TuiChip,
     TuiCardMedium,
+    TuiCardLarge,
     TuiHeader,
     TuiTitle,
     TuiAppearance,
     TuiBadge,
     NgxFlickeringGridComponent,
-    TuiAvatar
+    TuiAvatar,
+    NgxTimelineComponent,
+    NgxNeonUnderlineComponent
   ],
   templateUrl: './howitworks.component.html',
   styleUrl: './howitworks.component.less'
 })
 export class HowitworksComponent {
-
+  data: TimelineEntry[] = [
+    {
+      title: '<p>Start</p>',
+      content: '<div tuiAppearance="positive">The beginning of the project.<div>',
+    },
+    {
+      title: '<p>Development</p>',
+      content: '<div>Key development milestones achieved.</div>',
+    },
+    {
+      title: '<p>Launch</p>',
+      content: '<div>The project goes live!</div>',
+    },
+    {
+      title: '<p>end</p>',
+      content: '<div>The end of the project. The end of the project. The end of the project.lore. Lorem <div>',
+    },
+    
+  ];
 }
+
