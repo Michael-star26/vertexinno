17796bd043da8fd57ba90977d2cebc709948b850
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-04-18 02:57:29 +0300

    ui formatting

--- src/app/howitworks/howitworks.component.ts
+++ src/app/howitworks/howitworks.component.ts
@@ -27,24 +27,25 @@ import { NgxNeonUnderlineComponent } from '@omnedia/ngx-neon-underline';
 export class HowitworksComponent {
   data1: TimelineEntry[] = [
     {
-      title: '<p>Start</p>',
-      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">Headline</h1><p uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+      title: '<p>Tell Us What You Need</p>',
+      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">Planning</h1><p uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;">We start by understanding your goals. Whether it’s a website, mobile app, or custom system, we listen carefully and ask the right questions.</p></div></div>',
     },
     {
-      title: '<p>Development</p>',
-      content: '<div>Key development milestones achieved.</div>',
+      title: '<p>Get a Custom Plan</p>',
+      content: '<div>Based on your needs and budget, we provide a clear proposal with timelines, deliverables, and a strategy tailored to your business.</div>',
+      
     }
     
   ];
 
   data2: TimelineEntry[] = [
     {
-      title: '<p>Start</p>',
-      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">Headline</h1><p uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div>',
+      title: '<p> We Build & Test</p>',
+      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">Quality is our benchmark — nothing goes live until it’s perfect.</h1><p uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;">Our team designs, develops, and rigorously tests your solution — ensuring it’s reliable, scalable, and ready for real-world use.</p></div></div>',
     },
     {
-      title: '<p>end</p>',
-      content: '<div>The end of the project. The end of the project. The end of the project.lore. Lorem <div>',
+      title: '<p>Launch & Support</p>',
+      content: '<div>We deploy your product, provide training if needed, and offer ongoing support to keep everything running smoothly as your business grows. <div>',
     },
     
   ];
