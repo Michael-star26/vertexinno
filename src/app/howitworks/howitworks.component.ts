90d850a2193f17ad8fb3207143498136f57cb890
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-05-25 12:57:08 +0300

    some styling

--- src/app/howitworks/howitworks.component.ts
+++ src/app/howitworks/howitworks.component.ts
@@ -5,6 +5,7 @@ import { TuiCardLarge, TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
 import { NgxFlickeringGridComponent } from '@omnedia/ngx-flickering-grid';
 import { NgxTimelineComponent, TimelineEntry } from '@omnedia/ngx-timeline';
 import { NgxNeonUnderlineComponent } from '@omnedia/ngx-neon-underline';
+import { NgxShineBorderComponent } from '@omnedia/ngx-shine-border';
 
 @Component({
   selector: 'app-howitworks',
@@ -19,7 +20,8 @@ import { NgxNeonUnderlineComponent } from '@omnedia/ngx-neon-underline';
     NgxFlickeringGridComponent,
     TuiAvatar,
     NgxTimelineComponent,
-    NgxNeonUnderlineComponent
+    NgxNeonUnderlineComponent,
+    NgxShineBorderComponent
   ],
   templateUrl: './howitworks.component.html',
   styleUrl: './howitworks.component.less'
@@ -28,20 +30,20 @@ export class HowitworksComponent {
   data1: TimelineEntry[] = [
     {
       title: '<p class="timeline-title">1</p>',
-      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 class="timeline-title" uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">Tell Us What You Need</h1><p uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;" class="timeline-content">We start by understanding your goals. Whether it’s a website, mobile app, or custom system, we listen carefully and ask the right questions.</p></div></div>',
+      content: '<div><h1 class="timeline-title">Tell Us What You Need</h1><p class="timeline-content">We start by understanding your goals. Whether it’s a website, mobile app, or custom system, we listen carefully and ask the right questions.</p></div></div>',
     },
     {
       title: '<p class="timeline-title">2</p>',
-      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 class="timeline-title" uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">Get a Custom Plan</h1><p class="timeline-content" uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;" class="timeline-content">Based on your needs and budget, we provide a clear proposal with timelines, deliverables, and a strategy tailored to your business.</p></div></div>',
+      content: '<div><h1 class="timeline-title">Get a Custom Plan</h1><p  class="timeline-content">Based on your needs and budget, we provide a clear proposal with timelines, deliverables, and a strategy tailored to your business.</p></div>',
       
     },
     {
       title: '<p class="timeline-title">4</p>',
-      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 class="timeline-title" uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">We Build & Test</h1><p uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;" class="timeline-content">Our team designs, develops, and rigorously tests your solution — ensuring it’s reliable, scalable, and ready for real-world use.</p></div></div>',
+      content: '<div><h1 class="timeline-title">We Build & Test</h1><p class="timeline-content">Our team designs, develops, and rigorously tests your solution — ensuring it’s reliable, scalable, and ready for real-world use.</p></div></div>',
     },
     {
       title: '<p class="timeline-title">5</p>',
-      content: '<div class="uk-margin-auto uk-margin-auto-vertical"><h1 class="timeline-title" uk-parallax="opacity: 0,1; y: -100,0; scale: 2,1; end: 50vh + 50%;">Launch & Support</h1><p uk-parallax="opacity: 0,1; y: 100,0; scale: 0.5,1; end: 50vh + 50%;" class="timeline-content">We deploy your product, provide training if needed, and offer ongoing support to keep everything running smoothly as your business grows.</p></div></div>',
+      content: '<div ><h1 class="timeline-title" >Launch & Support</h1><p class="timeline-content">We deploy your product, provide training if needed, and offer ongoing support to keep everything running smoothly as your business grows.</p></div></div>',
     },
    
     
