a6ed94650dee0f74ab416e1563949a29c287b125
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-05-25 16:59:49 +0300

    some styling

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -5,6 +5,8 @@ import { TuiCardLarge, TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
 import { NgxTracingBeamComponent } from '@omnedia/ngx-tracing-beam';
 import { NgxShineBorderComponent } from '@omnedia/ngx-shine-border';
 import { NgxDotpatternComponent } from '@omnedia/ngx-dotpattern';
+import { NgxNeonUnderlineComponent } from '@omnedia/ngx-neon-underline';
+import { NgxShinyTextComponent } from '@omnedia/ngx-shiny-text';
 @Component({
   selector: 'app-services',
   imports: [
@@ -19,7 +21,9 @@ import { NgxDotpatternComponent } from '@omnedia/ngx-dotpattern';
     NgxTracingBeamComponent,
     NgxShineBorderComponent,
     NgxDotpatternComponent,
-    TuiScrollbar
+    TuiScrollbar,
+    NgxNeonUnderlineComponent,
+    NgxShinyTextComponent
   ],
   
   templateUrl: './services.component.html',
